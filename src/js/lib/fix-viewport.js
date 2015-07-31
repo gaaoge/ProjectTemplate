6b16eb0e5729ced055f1cdaf7ae1c78554968fa4
GG <723687948@qq.com>
2015-07-31 11:32:57 +0800

    优化调整通用样式。

--- src/js/lib/fix-viewport.js
+++ src/js/lib/fix-viewport.js
@@ -16,8 +16,8 @@
     function fixViewport (type, width) {
         var docEl = doc.documentElement;
         var metaEl = doc.querySelector('meta[name="viewport"]');
-        //由于页面初始设置了viewport的width=device-width,所以此处取得的即是屏幕的设备宽度
-        var clientWidth = docEl.clientWidth;
+        //由于页面初始设置了viewport的width=device-width,所以此处docEl.clientWidth即是屏幕的设备宽度（同时判断clientHeight以较小值为准）
+        var clientWidth = Math.min(docEl.clientWidth, docEl.clientHeight);
         var scale, content;
 
         switch (type) {
@@ -30,11 +30,6 @@
                 var dpr = win.devicePixelRatio || 1;
                 // 设置data-dpr属性，留作css hack设置元素的font-size用
                 docEl.setAttribute('data-dpr', dpr);
-
-                //限制clientWidth的最大值，用于在pc端以640的宽度展示页面
-                if (clientWidth > 640) {
-                    clientWidth = 640 / dpr;
-                }
                 docEl.style.fontSize = 100 * (clientWidth * dpr / width) + "px";
 
                 scale = 1 / dpr;
