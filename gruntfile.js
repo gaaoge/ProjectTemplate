bfe47e3ae562f77e8f132dd8581490902d901a01
GG <723687948@qq.com>
2015-08-17 19:36:24 +0800

    调整为使用grunt的方式进行测试和发布。

---
+++ gruntfile.js
@@ -0,0 +1,87 @@
+/**
+ * Created by GG on 15/8/17.
+ */
+
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        replace: {
+            dist: {
+                options: {
+                    patterns: [
+                        {
+                            match: 'href="css/index.min.css"',
+                            replacement: 'href="http://img<%=Math.ceil(Math.random() * 6) %>.cache.netease.com/utf8/3g/gg/' +
+                            '<%=pkg.name %>/css/index.min.css?v=<%=new Date().getTime() %>"'
+                        },
+                        {
+                            match: 'src="js/index.min.js"',
+                            replacement: 'src="http://img<%=Math.ceil(Math.random() * 6) %>.cache.netease.com/utf8/3g/gg/' +
+                            '<%=pkg.name %>/js/index.min.js?v=<%=new Date().getTime() %>"'
+                        }
+                    ],
+                    usePrefix: false
+                },
+                files: [{
+                    src: 'build/index.html',
+                    dest: 'build/index.html'
+                }]
+            }
+        },
+        ftps_deploy: {
+            test: {
+                options: {
+                    auth: {
+                        host: '220.181.8.33',
+                        port: '16321',
+                        authKey: 'test'
+                    }
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'build',
+                    src: ['**/*'],
+                    dest: '/gg/<%=pkg.name %>'
+                }]
+            },
+            publish_html: {
+                options: {
+                    auth: {
+                        host: '220.181.29.249',
+                        port: '16321',
+                        authKey: 'publish_html'
+                    }
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'build',
+                    src: ['*.html'],
+                    dest: '/gg/<%=pkg.name %>'
+                }]
+            },
+            publish_asset: {
+                options: {
+                    auth: {
+                        host: '61.135.251.132',
+                        port: '16321',
+                        authKey: 'publish_asset',
+                        secure: true
+                    }
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'build',
+                    src: ['**/*', '!*.html'],
+                    dest: '/utf8/3g/gg/<%=pkg.name %>'
+                }]
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-replace');
+    grunt.loadNpmTasks('grunt-ftps-deploy');
+
+    grunt.registerTask('test', ["ftps_deploy:test"]);
+    grunt.registerTask('publish', ["replace", "ftps_deploy:publish_html", "ftps_deploy:publish_asset"]);
+};
--- gulpfile.js
+++ gulpfile.js
@@ -7,17 +7,11 @@ var del = require('del');
 var usemin = require('gulp-usemin');
 var uglify = require('gulp-uglify');
 var minifyCss = require('gulp-minify-css');
-var replace = require('gulp-replace');
-var ftp = require('gulp-ftp');
-var gutil = require('gulp-util');
-var pkg = require('./package.json');
 
 var path = {
     index: 'src/index.html',
     asset: ['src/**/*', '!src/css/**/*', '!src/js/**/*', '!src/index.html'],
-    dest: 'build/',
-    test: 'build/**/*',
-    publish: 'build/index.html'
+    dest: 'build/'
 };
 
 gulp.task('clean', function (cb) {
@@ -39,36 +33,3 @@ gulp.task('usemin', ['clean'], function () {
 });
 
 gulp.task('default', ['copy', 'usemin']);
-
-//测试
-gulp.task('test', ['default'], function () {
-    return gulp.src(path.test)
-        .pipe(ftp({
-            host: '220.181.8.33',
-            port: '16321',
-            user: 'wangjun2012',
-            pass: 'wangjun2012',
-            remotePath: 'gg/' + pkg.name
-        }))
-        .pipe(gutil.noop());
-});
-
-/*
- * 发布（替换为线上地址并添加版本号，以及上传到ftp）
- */
-gulp.task('publish', ['default'], function () {
-    var projectPath = 'http://img' + Math.round(Math.random() * 6) + '.cache.netease.com/utf8/3g/gg/' + pkg.name;
-    var date = new Date().getTime();
-    return gulp.src(path.publish)
-        .pipe(replace('href="css/index.min.css"', 'href="' + projectPath + '/css/index.min.css?v=' + date + '"'))
-        .pipe(replace('src="js/index.min.js"', 'src="' + projectPath + '/js/index.min.js?v=' + date + '"'))
-        .pipe(gulp.dest(path.dest))
-        .pipe(ftp({
-            host: '220.181.29.249',
-            port: '16321',
-            user: 'wangjun2012',
-            pass: 'wangjun2012',
-            remotePath: 'gg/' + pkg.name
-        }))
-        .pipe(gutil.noop());
-});
