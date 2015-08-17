cb640bbfbcd7f067b8851820ee6dd2a6f801d105
GG <723687948@qq.com>
2015-08-17 10:47:42 +0800

    完善测试和发布的gulp配置。

--- gulpfile.js
+++ gulpfile.js
@@ -9,18 +9,15 @@ var uglify = require('gulp-uglify');
 var minifyCss = require('gulp-minify-css');
 var replace = require('gulp-replace');
 var ftp = require('gulp-ftp');
-var package = require('./package.json');
+var gutil = require('gulp-util');
+var pkg = require('./package.json');
 
 var path = {
-    pack: {
-        index: 'src/index.html',
-        asset: ['src/**/*', '!src/css/**/*', '!src/js/**/*', '!src/index.html']
-    },
-    publish: {
-        index: 'build/index.html',
-        asset: ['build/**/*', '!build/*.html']
-    },
-    dest: 'build/'
+    index: 'src/index.html',
+    asset: ['src/**/*', '!src/css/**/*', '!src/js/**/*', '!src/index.html'],
+    dest: 'build/',
+    test: 'build/**/*',
+    publish: 'build/index.html'
 };
 
 gulp.task('clean', function (cb) {
@@ -28,12 +25,12 @@ gulp.task('clean', function (cb) {
 });
 
 gulp.task('copy', ['clean'], function () {
-    return gulp.src(path.pack.asset)
+    return gulp.src(path.asset)
         .pipe(gulp.dest(path.dest));
 });
 
 gulp.task('usemin', ['clean'], function () {
-    return gulp.src(path.pack.index)
+    return gulp.src(path.index)
         .pipe(usemin({
             css: [minifyCss()],
             js: [uglify()]
@@ -43,36 +40,35 @@ gulp.task('usemin', ['clean'], function () {
 
 gulp.task('default', ['copy', 'usemin']);
 
-/*
- * 发布（替换为线上地址并添加版本号，以及上传到ftp）
- */
-gulp.task('replace', [], function () {
-    var projectPath = 'http://img5.cache.netease.com/utf8/3g/gg/' + package.name;
-    var date = new Date().getTime();
-    return gulp.src(path.publish.index)
-        .pipe(replace('href="css/index.min.css"', 'href="' + projectPath + '/css/index.min.css?v=' + date + '"'))
-        .pipe(replace('src="js/index.min.js"', 'src="' + projectPath + '/js/index.min.js?v=' + date + '"'))
-        .pipe(gulp.dest(path.dest));
-});
-
-gulp.task('publish', ['replace'], function () {
-    gulp.src(path.publish.index)
-        .pipe(ftp({
-            host: '220.181.29.249',
-            port: '16321',
-            user: 'wangjun2012',
-            pass: 'wangjun2012',
-            remotePath: 'gg/' + package.name + '/'
-        }));
-});
-
-gulp.task('test', ['replace'], function () {
-    gulp.src(path.publish.index)
+//测试
+gulp.task('test', ['default'], function () {
+    return gulp.src(path.test)
         .pipe(ftp({
             host: '220.181.8.33',
             port: '16321',
             user: 'wangjun2012',
             pass: 'wangjun2012',
-            remotePath: 'gg/' + package.name + '/'
-        }));
+            remotePath: 'gg/' + pkg.name
+        }))
+        .pipe(gutil.noop());
+});
+
+/*
+ * 发布（替换为线上地址并添加版本号，以及上传到ftp）
+ */
+gulp.task('publish', ['default'], function () {
+    var projectPath = 'http://img' + Math.round(Math.random() * 6) + '.cache.netease.com/utf8/3g/gg/' + pkg.name;
+    var date = new Date().getTime();
+    return gulp.src(path.publish)
+        .pipe(replace('href="css/index.min.css"', 'href="' + projectPath + '/css/index.min.css?v=' + date + '"'))
+        .pipe(replace('src="js/index.min.js"', 'src="' + projectPath + '/js/index.min.js?v=' + date + '"'))
+        .pipe(gulp.dest(path.dest))
+        .pipe(ftp({
+            host: '220.181.29.249',
+            port: '16321',
+            user: 'wangjun2012',
+            pass: 'wangjun2012',
+            remotePath: 'gg/' + pkg.name
+        }))
+        .pipe(gutil.noop());
 });
