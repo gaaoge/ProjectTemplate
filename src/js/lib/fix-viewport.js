e19cd13bcde957cf6dc1a8dc041c24e68edb9090
GG <723687948@qq.com>
2015-09-17 17:11:53 +0800

    优化字体适配。

--- src/js/lib/fix-viewport.js
+++ src/js/lib/fix-viewport.js
@@ -5,15 +5,16 @@
  *  参数：type(适配类型, 可选值为'fixed'或'rem')；width(设计稿宽度，整数)
  *  可以选择以下两种情况适配：
  *  1. 定宽：原理是设定viewport的宽度为固定值(即传递的width值)，并根据屏幕宽度和width值计算viewport缩放比例。
- *     实际开发在css中长度尺寸均使用px单位，大小设置为设计稿中原始尺寸值即可。
+ *     实际开发在css中任何长度尺寸均使用px单位，大小设置为设计稿中原始尺寸值；
+ *     字体单位使用rem, 大小为设计稿中px单位的尺寸值 / 100;
  *  2. REM：原理是根据设备dpr和屏幕宽度来计算viewport宽度，并根据设备dpr计算viewport缩放比例，然后在html标签中设置font-size的属性值。
  *     实际开发在css中任何长度尺寸均使用rem单位，大小设置为设计稿中px单位的尺寸值 / 100;
- *     字体单位不要用rem，使用px并根据data-dpr指定不同的字体大小；
+ *     字体单位使用px, 根据data-dpr指定不同的字体大小；
  *     若需要1px物理像素的边框效果，则border的宽度不需要使用rem，设定为1px即可。
  */
 
 (function (win, doc) {
-    function fixViewport (type, width) {
+    function fixViewport(type, width) {
         var docEl = doc.documentElement;
         var metaEl = doc.querySelector('meta[name="viewport"]');
         //由于页面初始设置了viewport的width=device-width,所以此处docEl.clientWidth即是屏幕的设备宽度（同时判断clientHeight以较小值为准）
@@ -25,6 +26,7 @@
                 scale = clientWidth / width;
                 content = 'width=' + width + ',initial-scale=' + scale + ',maximum-scale=' + scale +
                     ',minimum-scale=' + scale;
+                docEl.style.fontSize = 50 / scale + 'px';
                 break;
             case 'rem':
                 var dpr = win.devicePixelRatio || 1;
